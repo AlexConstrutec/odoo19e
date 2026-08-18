da6318dbd8634a6db44be00887fb460283192474
AlexConstrutec <alex.martinez@construtecasesores.com>
2026-08-18 15:57:15 -0600

    Revert "Agregar Solicitud de Pago (Viaticos) dual-edicion a construtec_account_payment_order_19"
    This reverts commit b1d74e524cf900914c01577e9a277dd139b5a1fd.

--- construtec_account_payment_order_19/models/__init__.py
+++ construtec_account_payment_order_19/models/__init__.py
@@ -1,7 +1,3 @@
 from . import account_payment_order
 from . import account_move
 from . import account_payment
-from . import res_company
-from . import res_config_settings
-from . import account_payment_order_sync_log
-from . import account_payment_order_request
